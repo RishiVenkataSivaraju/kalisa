f3135d8eec363d05b9d2a07ae2ae9b3c2d3ea84f
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-03-18 20:07:45 +0530

    Mongoose Schema

---
+++ module/UserDetails.js
@@ -0,0 +1,13 @@
+const mongoose= require("mongoose");
+
+const Schema = mongoose.Schema;
+ 
+const userDetails = new Schema({
+fromDate:String,
+toDate:String,
+gmail:String,
+phonenumber:Number
+})
+ 
+const USERDETAILS = mongoose.model("USERDETAILS",userDetails);
+module.exports=USERDETAILS;
