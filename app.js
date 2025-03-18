27fcca623471e09d633c0cd090401373dcd1229d
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-03-18 20:07:35 +0530

    Mongoose Schema

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path =require("path");
 const app = express();
+const USERDETAILS= require("./module/UserDetails");
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'styles')));
 app.use(express.static(path.join(__dirname, 'module')));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.post("/bookroom",(req,res) =>{
+ const {fromDate,toDate,gmail,phonenumber}= req.body;
+ console.log(fromDate);
+})
 
 app.get("/",(req,res)=>{
  res.render("home");
