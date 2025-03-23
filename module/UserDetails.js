0db91153fa7de96cd88d3ea44e26495c7b3847c9
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-03-23 15:38:30 +0530

    MONGO ATLAS CONNECTED

--- module/UserDetails.js
+++ module/UserDetails.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config(); 
+const accountSid = process.env.accountSid
+const authToken = process.env.authToken;
+const client = new twilio(accountSid,authToken)
+const adminPhoneNumber = process.env.adminPhoneNumber; 
+const twilioWhatsAppNumber = process.env.twilioWhatsAppNumber;
 const Schema = mongoose.Schema;
 const userDetails = new Schema({
     fromDate: String,
@@ -9,6 +14,20 @@ const userDetails = new Schema({
     noOfRooms: Number,
     people: Number
 })
+userDetails.post("save", async function (doc) {
+    try {
+        const message = `📢 New Room Booking!\n\n🛏️ Dates: ${doc.fromDate} - ${doc.toDate}\n📧 Email: ${doc.gmail}\n📞 Phone: ${doc.phonenumber} \n No of rooms : ${doc.noOfRooms} \n  People : ${doc.people}`;
 
+        await client.messages.create({
+            from: twilioWhatsAppNumber,
+            to: adminPhoneNumber,
+            body: message
+        });
+
+        console.log("WhatsApp notification sent to admin.");
+    } catch (error) {
+        console.error("Failed to send WhatsApp notification:", error);
+    }
+});
 const USERDETAILS = mongoose.model("USERDETAILS", userDetails);
 module.exports = USERDETAILS;
